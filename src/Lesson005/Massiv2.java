2631329dcfb480875baf0382168b20de64df0bce
VolkSteep <vadim6452@mail.ru>
2017-10-19 19:51:57 +0400

    .

--- src/Lesson005/Massiv2.java
+++ src/Lesson005/Massiv2.java
@@ -5,21 +5,22 @@ public class Massiv2 {
     public static void main(String[] args) {
         Random rnd = new Random();
         final int n = rnd.nextInt(10) + 1;
-        final int K = rnd.nextInt(10) + 1;
+        final int k = rnd.nextInt(10) + 1;
 
-        int[][] array = new int[n][K];
-        for (int i = 0; i < K; i++) {
+        int[][] array = new int[n][k];
+        for (int i = 0; i < k; i++) {
             for (int j = 0; j < n; j++) {
-                array[i][j] = rnd.nextInt(90) + 10;
+                array[i][j] = rnd.nextInt(10);
 
             }
         }
         System.out.println("Двумерный массив:");
-        for (int i = 0; i < K; i++) {
+        for (int i = 0; i < k; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
+
     }
 }
