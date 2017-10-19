38d7ebd242593f0344fd487d8802a05cfbc8cfd7
VolkSteep <vadim6452@mail.ru>
2017-10-19 19:26:14 +0400

    add Massiv

--- src/Lesson004/Massiv.java
+++ src/Lesson004/Massiv.java
@@ -1,29 +1,40 @@
 package Lesson004;
+
+import java.util.Random;
 import java.util.Scanner;
 class Massiv {
     public static void main(String [] args ){
         Scanner scn = new Scanner(System.in);
-        int n,a = 0,b = 0;
-        int k = 0;
-        int s = 0;
-        int[] arrayA;
-        System.out.println("Количество элементов в массиве = ");
+        Random rnd = new Random();
+        int n;
+        System.out.print("Кол-во элементов в массиве = ");
         n = scn.nextInt();
-        arrayA = new int [n];
-        for (int i = 0; i < 1; i++) {
-            System.out.println("Выбери первый элемент");
-            arrayA[k] = scn.nextInt();
-            a = arrayA[k];
-            System.out.println("Выбери второй элемент");
-            arrayA[s] = scn.nextInt();
-            b = arrayA[s];
+        int [] arrayA = new int[n];
+
+        for(int i=0; i < n; i++) {
+            arrayA[i] = rnd.nextInt(10)+1;
+
         }
-        for (int i = 0; i < 1; i++) {
-            if ( a > b ){
+        System.out.println("Вывод массива");
+        for (int i=0; i < n; i++) {
+            System.out.print(arrayA[i] + " ");
+        }
+        System.out.println("\nВыбери первый элемент");
+        int k = scn.nextInt();
+        k = k-1;
+        System.out.println("Выбери второй элемент");
+        int s = scn.nextInt();
+        s = s - 1;
+        System.out.println("Введенные элементы: " +arrayA[k]+" "+arrayA[s]);
+        for(int i=0; i < 1; i++){
+            if(arrayA[k] > arrayA[s]){
                 System.out.println("Первый элемент больше");
-            } else {
+            }else {
                 System.out.println("Второй элемент больше");
             }
+
         }
+
     }
 }
+
