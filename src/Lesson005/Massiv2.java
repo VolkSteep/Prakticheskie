0c5ffa7406e84351561f692a360795649a57542f
VolkSteep <vadim6452@mail.ru>
2017-10-30 17:34:24 +0300

    .

--- src/Lesson005/Massiv2.java
+++ src/Lesson005/Massiv2.java
@@ -4,19 +4,18 @@ import java.util.Random;
 public class Massiv2 {
     public static void main(String[] args) {
         Random rnd = new Random();
-        final int n = rnd.nextInt(10) + 1;
-        final int k = rnd.nextInt(10) + 1;
+        int n = rnd.nextInt(10)+1;
+        int m = n;
 
-        int[][] array = new int[n][k];
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < n; j++) {
-                array[i][j] = rnd.nextInt(10);
-
+        int[][] array = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                array[i][j] = rnd.nextInt(90) + 10;
             }
         }
         System.out.println("Двумерный массив:");
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < n; j++) {
+        for(int i=0; i < n; i++){
+            for (int j=0; j < n;j++ ){
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
--- src/Lesson008/File.java
+++ src/Lesson008/File.java
@@ -1,11 +1,15 @@
 package Lesson008;
 
-import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.FileReader;
 
 class File{
     public static void main(String[] args) throws Exception {
+        FileWriter fw = new FileWriter("C://proga/text.txt");
+        fw.append("\n");
+        fw.append ("До свидания, люди!");
+        fw.close();
+
 
 
     }
